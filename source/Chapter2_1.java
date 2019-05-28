a25d5300321a5770773edddf012b7233f9a040ca
master <hwk0911@gmail.com>
2019-05-28 21:22:42 +0900

    ADD : Chapter 2 source

---
+++ source/Chapter2_1.java
@@ -0,0 +1,21 @@
+public class Chapter2_1 {
+    public static void main(String[] args){
+        int a,b;
+        int result;
+
+        a = 100;
+        b = 50;
+
+        result = a+ b;
+        System.out.println(a + "+" + b + "=" + result);
+
+        result = a - b;
+        System.out.println(a + "-" + b + "=" + result);
+
+        result = a * b;
+        System.out.println(a + "*" + b + "=" + result);
+
+        result = a / b;
+        System.out.println(a + "/" + b + "=" + result);
+    }
+}
---
+++ source/Chapter2_2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Chapter2_2 {
+    public static void main(String[] args){
+        int a,b;
+        int result;
+
+        Scanner s = new Scanner(System.in);
+
+        a = s.nextInt();
+        b = s.nextInt();
+
+        result = a+ b;
+        System.out.println(a + "+" + b + "=" + result);
+
+        result = a - b;
+        System.out.println(a + "-" + b + "=" + result);
+
+        result = a * b;
+        System.out.println(a + "*" + b + "=" + result);
+
+        if(b != 0) {
+            result = a / b;
+            System.out.println(a + "/" + b + "=" + result);
+        }
+    }
+}
